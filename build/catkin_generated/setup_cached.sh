1e8233feaa79f3a1901a1b2e3c89640bc33d6f20
hong2244_linux <ho2n8g.er.tong@gmail.com>
2021-09-15 23:43:40 +0800

    update lesson two add C_one

--- build/catkin_generated/setup_cached.sh
+++ build/catkin_generated/setup_cached.sh
@@ -8,6 +8,8 @@
 
 # modified environment variables
 export CMAKE_PREFIX_PATH="/home/hong2244/test/devel:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH="/home/hong2244/test/devel/lib:$LD_LIBRARY_PATH"
+export PKG_CONFIG_PATH="/home/hong2244/test/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
 export PWD='/home/hong2244/test/build'
 export ROSLISP_PACKAGE_DIRECTORIES="/home/hong2244/test/devel/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
 export ROS_PACKAGE_PATH="/home/hong2244/test/src:$ROS_PACKAGE_PATH"
